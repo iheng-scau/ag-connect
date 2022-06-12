ad1b791a0d50350c7dac9851b4b436b98b3f4575
iheng <iheng_scau@hotmail.com>
2022-06-12 23:35:34 +0800

    完成了部分页面的开发

---
+++ pages/index/splash/splash.js
@@ -0,0 +1,14 @@
+// pages/index/splash/splash.js
+Page({
+  data: {
+
+  },
+
+  onShow() {
+    setTimeout(function(){
+      wx.navigateTo({
+        url: '/pages/index/index',
+      })
+    }, 2000);
+  }
+})
