04e0972495f12951b1db182523e9a274c0b5bf35
iheng <iheng_scau@hotmail.com>
2022-06-18 15:37:17 +0800

    完成了闪屏的开发

--- pages/splash/splash.js
+++ pages/splash/splash.js
@@ -1,14 +1,27 @@
 // pages/index/splash/splash.js
 Page({
   data: {
-
+    logoSrc : "../../images/ag-palace.png",
+    styleClass: "fa-solid fa-building-columns"
   },
 
   onShow() {
-    // setTimeout(function(){
-    //   wx.navigateTo({
-    //     url: '/pages/index/index',
-    //   })
-    // }, 2000);
+    this.animate("#logo", [
+      {opacity:100, ease: 'ease-out'},
+      {opacity:0}
+    ], 2000, () => {
+      this.setData({
+        logoSrc : "../../images/twc.png",
+        styleClass: "fa-solid fa-star"
+      });
+      this.animate("#logo", [
+        {opacity:0},
+        {opacity:100, ease: 'ease-out'}
+      ], 2000, function(){
+        wx.navigateTo({
+           url: '/pages/index/index',
+          })
+      })
+    })
   }
 })
